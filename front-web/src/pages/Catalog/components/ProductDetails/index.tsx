6650f199214a6768aae0e776617c2020c4389c3a
nabucodonosor-java <nabucodonosor.java@gmail.com>
2021-06-29 11:03:21 -0300

    Add layout 576px in ProductFilters, Catalog and ProductDetails

--- front-web/src/pages/Catalog/components/ProductDetails/index.tsx
+++ front-web/src/pages/Catalog/components/ProductDetails/index.tsx
@@ -8,7 +8,7 @@ import ProductDescriptionLoader from '../Loaders/ProductDescriptionLoader';
 import ProductInfoLoader from '../Loaders/ProductInfoLoader';
 import './styles.scss';
 
-type ParamsType = { 
+type ParamsType = {
     productId: string;
 }
 
@@ -16,50 +16,50 @@ const ProductDetails = () => {
 
     const { productId } = useParams<ParamsType>();
     const [product, setProduct] = useState<Product>();
-    const[isLoading, setIsLoading] = useState(false);
-    
+    const [isLoading, setIsLoading] = useState(false);
+
     useEffect(() => {
         setIsLoading(true);
-        makeRequest({ url: `/products/${productId}`})
-        .then(response => setProduct(response.data))
-        .finally(() => setIsLoading(false));
+        makeRequest({ url: `/products/${productId}` })
+            .then(response => setProduct(response.data))
+            .finally(() => setIsLoading(false));
     }, [productId]);
 
     return (
         <div className="product-details-container">
             <div className="card-base border-radius-20 product-details">
                 <Link to="/products" className="product-details-goback">
-                <ArrowIcon className="icon-goback"/>
-                <h1 className="text-goback">voltar</h1>
+                    <ArrowIcon className="icon-goback" />
+                    <h1 className="text-goback">voltar</h1>
                 </Link>
                 <div className="product-details-info">
-                    <div className="col-6 pr-5">
-                        {isLoading ? <ProductInfoLoader /> : (
-                            <>
+
+                    {isLoading ? <ProductInfoLoader /> : (
+                        <>
                             <div className="product-details-card text-center">
-                                 <img src={product?.imgUrl} alt={product?.name} className="product-details.image" />
+                                <img src={product?.imgUrl} alt={product?.name} className="product-details.image" />
                             </div>
                             <div className="product-info-fields">
                                 <h1 className="product-details-name">
-                                     {product?.name}
+                                    {product?.name}
                                 </h1>
-                            { product?.price && <ProductPrice price={product?.price} />}
+                                {product?.price && <ProductPrice price={product?.price} />}
                             </div>
-                               
-                            </>
-                        )}   
-                    </div>
+
+                        </>
+                    )}
+
                     <div className="product-details-card">
                         {isLoading ? <ProductDescriptionLoader /> : (
-                              <>
-                              <h1 className="product-description-title">
-                                   Descrição do produto
-                                   </h1>
-                               <p className="product-description-text">
-                                   {product?.description}
-                               </p>
-                              </>
-                        )}   
+                            <>
+                                <h1 className="product-description-title">
+                                    Descrição do produto
+                                </h1>
+                                <p className="product-description-text">
+                                    {product?.description}
+                                </p>
+                            </>
+                        )}
                     </div>
                 </div>
             </div>
