e4f91ead6c63d7b9d3d17d876de46fe78774fe65
nabucodonosor-java <nabucodonosor.java@gmail.com>
2021-06-29 10:42:44 -0300

    Add layout 576px in Home and ButtonIcon

--- front-web/src/core/components/ButtonIcon/index.tsx
+++ front-web/src/core/components/ButtonIcon/index.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 import { ReactComponent as ArrowIcon } from '../../assets/images/arrow.svg';
 import './styles.scss';
 
-type Props = {
+type Props = { 
     text: string;
 }    
 
 const ButtonIcon = ({ text }: Props) => (
-    <div className="d-flex">
+    <div className="default-btn">
         <button className="btn btn-primary btn-icon">
         <h5>{text}</h5>
     </button>
