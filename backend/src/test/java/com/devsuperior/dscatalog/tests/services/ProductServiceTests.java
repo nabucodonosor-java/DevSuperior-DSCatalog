73211f6225bf1be4e5597272f84c956aa6ed19a3
nabucodonosor-java <nabucodonosor.java@gmail.com>
2021-07-30 19:14:44 -0300

    Class 06-28

--- backend/src/test/java/com/devsuperior/dscatalog/tests/services/ProductServiceTests.java
+++ backend/src/test/java/com/devsuperior/dscatalog/tests/services/ProductServiceTests.java
@@ -1,17 +1,23 @@
 package com.devsuperior.dscatalog.tests.services;
 
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.verify;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.devsuperior.dscatalog.repositories.ProductRepository;
 import com.devsuperior.dscatalog.services.ProductService;
+import com.devsuperior.dscatalog.services.exceptions.DatabaseException;
+import com.devsuperior.dscatalog.services.exceptions.NotFoundException;
 
 @ExtendWith(SpringExtension.class)
 public class ProductServiceTests {
@@ -24,14 +30,37 @@ public class ProductServiceTests {
 	
 	private long existingId;
 	private long nonExistingId;
+	private long dependentId;
 	
 	@BeforeEach
 	void setUp() {
 		existingId = 1L;
 		nonExistingId = 9999L;
+		dependentId = 4L;
 		
-		Mockito.doNothing().when(repository).deleteById(existingId);
-		Mockito.doThrow(EmptyResultDataAccessException.class).when(repository).deleteById(nonExistingId);
+		doNothing().when(repository).deleteById(existingId);
+		doThrow(EmptyResultDataAccessException.class).when(repository).deleteById(nonExistingId);
+		doThrow(DataIntegrityViolationException.class).when(repository).deleteById(dependentId);
+	}
+	
+	@Test
+	public void deleteShouldThrowDatabaseExceptionWhenIdDoesNotExists() {
+		
+		Assertions.assertThrows(DatabaseException.class, () -> {
+			service.delete(dependentId);
+		});
+		
+		verify(repository).deleteById(dependentId);
+	}
+	
+	@Test
+	public void deleteShouldThrowNotFoundExceptionWhenIdDoesNotExists() {
+		
+		Assertions.assertThrows(NotFoundException.class, () -> {
+			service.delete(nonExistingId);
+		});
+		
+		verify(repository).deleteById(nonExistingId);
 	}
 	
 	@Test
@@ -41,6 +70,6 @@ public class ProductServiceTests {
 			service.delete(existingId);
 		});
 		
-		Mockito.verify(repository).deleteById(existingId);
+		verify(repository).deleteById(existingId);
 	}
 }
